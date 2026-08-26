ee172da4e5cc2c461ee8826e88c30f70b3f4adf2
golvrr <47479437811@info.local>
2026-08-26 17:53:48 -0300

    aula 2 js

---
+++ js/script.js
@@ -0,0 +1,169 @@
+// == compara valor
+// === compara valor e tipo
+// = atribuicao
+
+
+//let numero1 = Number(prompt("primeiro numero"))
+//let numero2 = Number(prompt("segundo numero"))
+// o prompt converte para texto
+
+
+//colocar () se o codigo tiver mais de uma 
+
+//let media = (numero1 + numero2) / 2 
+
+// PRIMEIRO EXERCICIO
+//let idade = Number(prompt("Qual sua Idade?"));
+
+//if (idade >= 18) {
+//    console.log("Maior de idade");
+//} else {
+   //console.log("Menor de idade");
+//}
+//let numero = Number(prompt("Qual sua Idade?"));
+
+//if (numero > 18) {
+ //   console.log("Positivo");
+//} else {
+   //console.log("Negativo");
+//}
+
+// SEGUNDO EXERCICIO
+
+//let numero = Number(prompt("Digite um número:"));
+
+//if (numero > 0) {
+ //   console.log("Positivo");
+//} else {
+  //  console.log("Negativo");
+//}
+
+// TERCEIRO EXERCICIO
+
+//let nota = Number(prompt("Digite a nota final do aluno (0 a 100):"));
+
+//if (nota >= 60) {
+  //  console.log("Aprovado");
+//} else {
+  //  console.log("Reprovado");
+//}
+
+// QUARTO EXERCICIO
+
+//let numero = Number(prompt("Digite um número:"));
+
+//if (numero > 0) {
+  //  console.log("Positivo");
+//} else if (numero < 0) {
+  //  console.log("Negativo");
+//} else {
+  //  console.log("Zero");
+//}
+
+// QUINTO EXERCICIO
+
+//let idade = Number(prompt("Digite sua idade:"));
+
+//if (idade >= 0 && idade <= 12) {
+  //  console.log("Criança");
+
+//} else if (idade >= 13 && idade <= 17) {
+  //  console.log("Adolescente");
+
+//} else {
+  //  console.log("Adulto");
+
+//}
+    
+// SEXTO EXERCICIO
+
+//const numero = parseInt(prompt("Digite um número inteiro:"), 10);
+
+//if (numero % 2 === 0) {
+  //console.log("O número é par.");
+//} else {
+  //console.log("O número é ímpar.");
+//}
+
+// SETIMO EXERCICIO
+
+//let primeiro = Number(prompt("Digite o primeiro número:"));
+//let operacao = prompt("Digite a operação (+, -, * ou /):");
+//let segundo = Number(prompt("Digite o segundo número:"));
+
+//let resultado;
+
+//if (operacao === "+") {
+  //  resultado = primeiro + segundo;
+
+//} else if (operacao === "-") {
+   // resultado = primeiro - segundo;
+
+//} else if (operacao === "*") {
+  //  resultado = primeiro * segundo;
+
+//} else if (operacao === "/") {
+  //  resultado = primeiro / segundo;
+
+//} else {
+  //  alert("Erro: operação inválida.");
+//}
+
+//if (resultado !== undefined) {
+  //  alert("Resultado: " + resultado);
+//}
+
+// OITAVO EXERCICIO
+
+//let n1 = Number(prompt("Digite o primeiro número:"));
+//let n2 = Number(prompt("Digite o segundo número:"));
+//let n3 = Number(prompt("Digite o terceiro número:"));
+
+//let maior;
+
+//if (n1 > n2 && n1 > n3) {
+  //  maior = n1;
+//} else if (n2 > n1 && n2 > n3) {
+  //  maior = n2;
+//} else {
+  //  maior = n3;
+//}
+
+//console.log("O maior número é:", maior);
+
+// DECIMO EXERCICIO
+
+//const usuarioCorreto = "admin";
+//const senhaCorreta = "6767";
+
+//const usuario = prompt("Digite o nome de usuário:");
+//const senha = prompt("Digite a senha:");
+
+//if (usuario === usuarioCorreto && senha === senhaCorreta) {
+  //  alert("Login bem-sucedido!");
+//} else {
+  //  alert("Acesso Negadp.");
+//}
+
+// DECIMO PRIMEIRO EXERCICIO
+
+const nota = Number(prompt("Digite uma nota entre 0 e 100:"));
+
+let conceito;
+
+if (nota >= 90 && nota <= 100) {
+  conceito = "A";
+} else if (nota >= 80) {
+  conceito = "B";
+} else if (nota >= 70) {
+  conceito = "C";
+} else if (nota >= 60) {
+  conceito = "D";
+} else if (nota >= 0) {
+  conceito = "F";
+} else {
+  conceito = "Nota inválida";
+}
+
+console.log(`Conceito: ${conceito}`);
+
